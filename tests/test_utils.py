4d81a029348cb72168ede1c55f508995e6466419
HN <nhai.qn@gmail.com>
2019-03-23 02:02:49 -0400

    more

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,15 @@
 import os
 from pdb4amber import utils
-from mock import patch
+from mock import patch, MagicMock
 
 
 @patch('parmed.load_file')
 def test_get_amber_compatible_resnames(mock_load_file):
     mock_load_file.return_value = {'G3': 'mock'}
-    with patch.dict(os.environ, {'AMBERHOME':'/tmp/amber'}):
+
+    with patch('os.getenv', MagicMock(return_value='/tmp/amber/')), \
+            patch('os.path.exists', MagicMock(return_value=True)):
          assert utils.get_amber_compatible_resnames() == {'G3'}
-    with patch.dict(os.environ, {}):
+
+    with patch('os.getenv', MagicMock(return_value=None)):
          assert utils.get_amber_compatible_resnames() == set()
